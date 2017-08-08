8b7cc10d5b4005a32545977daf0c7b5a9ecba17d
Jackie Anne Robson <jackie.anne.robson@gmail.com>
2017-08-08 13:57:12 +0100

    de-bugged
    de-bugged the filter issues i was having

--- js/components/ViewModel.js
+++ js/components/ViewModel.js
@@ -1,19 +1,19 @@
-var menuBtn = $('#menuBtn');
-var menu = $('#menu');
-var mapContainer = $('#map');
-var filterForm = $('#filterForm');
+var menuBtn = $( '#menuBtn' );
+var menu = $( '#menu' );
+var mapContainer = $( '#map' );
+var filterForm = $( '#filterForm' );
 
-var openMenu = function(){
-	menu.toggleClass('open');
-	mapContainer.toggleClass('open');
+var openMenu = function() {
+	menu.toggleClass( 'open' );
+	mapContainer.toggleClass( 'open' );
 };
 
-var hide = function(){
-	filterForm.toggleClass('hide');
+var hide = function() {
+	filterForm.toggleClass( 'hide' );
 };
 
-var listClick = function(e){
-	google.maps.event.trigger(e, 'click');
+var listClick = function( e ) {
+	google.maps.event.trigger( e, 'click' );
 };
 
 
@@ -28,67 +28,53 @@ function ViewModel() {
 	self.open = openMenu;
 	self.center = listClick;
 	this.hideForm = hide;
-	self.markers = ko.observableArray([]);
-	self.filter = ko.observable('');
+	self.markers = ko.observableArray( [] );
+	self.filter = ko.observable( '' );
 	self.filterItems = ko.utils.arrayFilter();
-	self.initMarkers = function initMarkers(){
- 	for ( var i = 0; i < locations.length; i++ ) {
- 		var position = locations[ i ].location;
- 		var title = locations[ i ].title;
- 		var content = locations[i].content;
- 		var type = locations[i].type;
- 		var center = locations[i].center;
- 		var search = locations[i].search;
- 		var nyTimesQuery = locations[i].nyTimesQuery;
+	self.initMarkers = function initMarkers() {
+		for ( var i = 0; i < locations.length; i++ ) {
+			var position = locations[ i ].location;
+			var title = locations[ i ].title;
+			var content = locations[ i ].content;
+			var type = locations[ i ].type;
+			var center = locations[ i ].center;
+			var search = locations[ i ].search;
+			var nyTimesQuery = locations[ i ].nyTimesQuery;
 
- 		var marker = new google.maps.Marker( {
- 			position: position,
- 			map: map,
- 			title: title,
- 			animation: google.maps.Animation.DROP,
- 			id: i,
- 			content : content,
- 			type : type,
- 			search : search,
- 			nyTimesQuery : nyTimesQuery
- 		}
- 	);
- 		marker.addListener( 'click', populateInfoWindow( this, infowindow );
-  	);
- 		self.markers.push( marker );
- 	}
-};
+			var marker = new google.maps.Marker( {
+				position: position,
+				map: map,
+				title: title,
+				animation: google.maps.Animation.DROP,
+				id: i,
+				content: content,
+				type: type,
+				search: search,
+				nyTimesQuery: nyTimesQuery
+			} );
+			marker.addListener( 'click', function(){
+				populateInfoWindow( this, infowindow );
+			} );
+			self.markers.push( marker );
+		}
+	};
 
-<<<<<<< HEAD
-	self.filteredItems = ko.computed(function(){ko.utils.arrayFilter(self.markers(), function(marker){
-    if (marker.title.toLowerCase().indexOf(self.filter().toLowerCase()) !== -1) {
-      if (marker) {
-        marker.setMap(map);
-      }
-    } else if (marker) {
-      marker.setMap(null);
-    }
-    return marker.title.toLowerCase().indexOf(self.filter().toLowerCase()) !== -1;
-  })}, self);
+	self.filteredItems = ko.computed( () => ko.utils.arrayFilter( self.markers(), ( marker ) => {
+		// Check if title matches search
+		if ( marker.title.toLowerCase().indexOf( self.filter().toLowerCase() ) !== -1 ) {
+			// Display matching Markers
+			if ( marker ) {
+				marker.setMap( map );
+			}
+		} else if ( marker ) {
+			marker.setMap( null );
+		}
+		return marker.title.toLowerCase().indexOf( self.filter().toLowerCase() ) !== -1;
+	} ), self );
 
 	self.initMarkers();
-=======
-	self.filteredItems = ko.computed(function (location) {
-	var filter = self.filter();
-		if (!filter) {
-				return self.markers();
-		} else {
-				var filtered = ko.utils.arrayFilter(self.markers, function (location) {
-						return location === filter;
-				});
-
-				return self.markers = filtered
-		}
-	})
-	self.initMarkers()
->>>>>>> parent of 2d5b6f3... Filter Fix and README
 
 }
 
 
-ko.applyBindings(new ViewModel());
+ko.applyBindings( new ViewModel() );
--- js/components/mapControl.js
+++ js/components/mapControl.js
@@ -1,7 +1,7 @@
 var map;
 
 
-var infowindow = new google.maps.InfoWindow()
+var infowindow = new google.maps.InfoWindow();
 
 function initMap() {
 	map = new google.maps.Map( document.getElementById( 'map' ), {
@@ -11,7 +11,7 @@ function initMap() {
 		},
 		zoom: 14
 	} );
-};
+}
 
 var populateInfoWindow = function( marker, infowindow ) {
 	if ( infowindow.marker != marker ) {
@@ -20,9 +20,9 @@ var populateInfoWindow = function( marker, infowindow ) {
 		var streetViewLocation = marker.search;
 		var streetView = 'http://maps.googleapis.com/maps/api/streetview?size=200x100&location='+streetViewLocation+'&key=AIzaSyCPQHFprpz7RDVGh5OymViX72s7XRCgQHs';
 
-		marker.setAnimation(google.maps.Animation.BOUNCE)
+		marker.setAnimation(google.maps.Animation.BOUNCE);
 		setTimeout(function() {
-			infowindow.marker.setAnimation(null)},800)
+			infowindow.marker.setAnimation(null);},800);
 
 		infowindow.setContent( '<img class="infowindowImg" src='+streetView+'><h5>'+marker.title+ '<h5><div>' + marker.content + '</div> <ul id="articles"></ul>'  );
 		infowindow.open( map, marker );
@@ -42,29 +42,24 @@ var populateInfoWindow = function( marker, infowindow ) {
 				var articles = data.response.docs;
 				for(var i=0; i < 2; i++){
 					var article = articles[i];
-					$('#articles').append('\n\
-					<li class="article"> \n\
-						<a href="'+article.web_url+'">'+article.headline.main+'</a> \n\
-						<p>'+article.snippet+'</p> \n\
-					</li> \n\
-					');
+					$('#articles').append('<li class="article"><a href="'+article.web_url+'">'+article.headline.main+'</a><p>'+article.snippet+'</p></li>');
 				}
 			},
 			error : function(){
-				$("#articles").replaceWith('<h3 class="nytimes-header"> Data could not load! </h3>')
+				$("#articles").replaceWith('<h3 class="nytimes-header"> Data could not load! </h3>');
 			}, fail : function(err) {
 				throw err;
 			}
 
-		})
+		});
 		infowindow.addListener( 'closeclick', function() {
 			infowindow.close();
-		} )
+		} );
 	}
-}
+};
 
 
 
 
 
-initMap()
+initMap();
